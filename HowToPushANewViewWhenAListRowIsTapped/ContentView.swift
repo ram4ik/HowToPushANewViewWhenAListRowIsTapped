74256eb9df602a72d78087a3a4b51a2e73bb5af4
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-07 07:32:29 +0300

    init

--- HowToPushANewViewWhenAListRowIsTapped/ContentView.swift
+++ HowToPushANewViewWhenAListRowIsTapped/ContentView.swift
@@ -8,9 +8,40 @@
 
 import SwiftUI
 
+struct Restaurant: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+struct RestaurantView: View {
+    var restaurant: Restaurant
+
+    var body: some View {
+        Text("Come and eat at \(restaurant.name)")
+            .font(.largeTitle)
+    }
+}
+
+struct RestaurantRow: View {
+    var restaurant: Restaurant
+
+    var body: some View {
+        Text(restaurant.name)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        let first = Restaurant(name: "Joe's Original")
+        let restaurants = [first]
+
+        return NavigationView {
+            List(restaurants) { restaurant in
+                NavigationLink(destination: RestaurantView(restaurant: restaurant)) {
+                    RestaurantRow(restaurant: restaurant)
+                }
+            }.navigationBarTitle("Select a restaurant")
+        }
     }
 }
 
